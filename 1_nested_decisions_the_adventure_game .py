fc84b12ddcc980af0f4c406c2f4cbaebb1f2ef05
MArney86 <matthew.h.arney.1986@gmail.com>
2024-04-29 19:19:51 -0500

    adding assignment source files

---
+++ 1_nested_decisions_the_adventure_game .py
@@ -0,0 +1,47 @@
+#Task 1: Code Correction
+place = input("Choose a place: forest or cave? ")
+
+if place == "forest": #changed from assignment operator to equal to operator
+    action = input("climb a tree or cross a river? ")
+    if action == "climb a tree": #changed from assignment operator to equal to operator
+        print("You found a bird's nest!")
+    elif action == "cross a river": #changed from assignment operator to equal to operator and changed to elif, else doesn't take conditional statements
+        print("You found a boat!")
+elif place == "cave": #changed from assignment operator to equal to operator
+    print("You find a hidden treasure!")
+#Task 2: Setting the Scene
+place = input("Choose a place: forest or cave? ")
+
+if place == "forest":
+    action = input("climb a tree or cross a river?")
+    if action == "climb a tree":
+        print("You found a bird's nest!")
+    elif action == "cross a river":
+        print("You found a boat!")
+elif place == "cave":
+    action = input("light a torch or proceed in the dark? ")
+    if action == "light a torch": #added secondary conditions for the cave environment
+        print("You see a shiny spot in the cave and find a hidden treasure!") #if you light the torch you find a shiny treasure
+    elif action == "proceed in the dark":
+        print("You feel around blindly and find nothing in the cave") #if you go blind you're unable to find anything
+#Task 3: Default Path
+place = input("Choose a place: forest or cave? ")
+
+if place == "forest": 
+    action = input("climb a tree or cross a river?")
+    if action == "climb a tree":
+        print("You found a bird's nest!")
+    elif action == "cross a river":
+        print("You found a boat!")
+    else:
+        pass #added as placeholder for future development
+elif place == "cave":
+    action = input("light a torch or proceed in the dark? ")
+    if action == "light a torch":
+        print("You see a shiny spot in the cave and find a hidden treasure!")
+    elif action == "proceed in the dark":
+        print("You feel around blindly and find nothing in the cave")
+    else:
+        pass #added as placeholder for future development
+else:
+    pass #added as placeholder for future development
---
+++ 2_quick_decisions_the_event_planner.py
@@ -0,0 +1,18 @@
+#Task 1: Code Correction
+#You are provided with a Python script that is supposed to help in event planning, but it has errors. Identify and fix them.
+#Buggy Code:
+
+attendees = int(input("Enter number of attendees: ")) #typecasting needed for number inputs
+venue = "large hall" if attendees > 100 else "conference room"
+print(venue)
+
+#Task 2: Venue Selection
+
+recommendations = "We recommend requesting audio equipment and a projector" if attendees > 100 else "we have no equipment recommendations at this time" #recommend additional facilities like "audio system" or "projector" based on the number of attendees.
+print(recommendations)
+
+#Task 3: Catering Choices
+
+veggie = input("Do you want vegetarian food?(yes/no) ")#Ask the user if they want "vegetarian" food.
+caterers = "Veggie Delight Caterers" if veggie == "yes" else "Gourmet Meals Caterer" # Recommend "Veggie Delight Caterers" if yes, otherwise recommend "Gourmet Meals Caterers".
+print("We recommend " + caterers + " for any catering needs you may have.")
